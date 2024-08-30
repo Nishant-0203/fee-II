cb97a5c15c77f6b84eb2d4dd05395082ebd5a8b6
nishant-2005 <nishant2082.be23@chitkara.edu.in>
2024-08-30 14:26:38 +0530

    work

---
+++ class work/datatypes.js
@@ -0,0 +1,28 @@
+//1. number
+
+let a=10;
+let b=-5;
+let c=2.5;
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+
+//2. string
+
+let name="Nishu"
+console.log(typeof(name))
+
+//3. boolean
+
+let isAdmin=true;
+console.log(typeof(isAdmin))
+
+// undefined
+let y;
+console.log(typeof(y))
+y=20;
+console.log(typeof(y))
+
+// null
+let z=null;
+console.log(typeof(z))
---
+++ class work/variable.js
@@ -0,0 +1,27 @@
+//var
+// var a=10;
+// console.log(a)
+// var a=20;
+// console.log(a)
+// var a=30;
+// console.log(a)
+
+
+//let 
+// let a=10;
+// console.log(a)
+// let a=20;
+// console.log(a)
+// let a=30;
+// console.log(a)
+
+
+//const 
+const a=10;
+console.log(a)
+// const a=20;
+// console.log(a) //error
+// const a=30;
+// console.log(a) //error
+a=30;
+console.log(a)
